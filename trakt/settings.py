92f609d1257be57b38296ccb710d8b64dca921ea
Scott Campbell <scott.m.campbell11@gmail.com>
2017-10-25 21:23:42 -0400

    created the header structure. styled the login form with bootstrap app.

--- trakt/settings.py
+++ trakt/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'bootstrap3',
     'webpack_loader',
     'django_cleanup',
     'main',
