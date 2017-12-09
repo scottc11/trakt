0ac2628762cc222cfe51293e8fbb72bcbd77bace
Scott Campbell <scott.m.campbell11@gmail.com>
2017-12-09 18:54:33 -0500

    added selected file in list for track detail.

--- assets/js/components/trackDetailFileList.js
+++ assets/js/components/trackDetailFileList.js
@@ -9,8 +9,9 @@ class TrackDetailFileList extends Component {
 
     const files = this.props.files.map( (file) => {
       let date = file.pub_date.split('T')[0]
+      let style = this.props.active == file.id ? 'track__file-list--active' : '';
       return (
-          <li key={file.id} onClick={ () => this.props.onClick(file) }>
+          <li className={ style } key={file.id} onClick={ () => this.props.onClick(file) }>
             <span className="fa fa-play-circle"></span> {file.title}
             <span className="track__file-list--date-time"> - {date}</span>
             <a href={ file.file.url }><span className="track__file-list--action fa fa-cloud-download"></span></a>
