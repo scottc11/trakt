36ec96ee4702c61eb23223159cdc5db9f61653fa
Scott Campbell <scott.m.campbell11@gmail.com>
2018-01-25 13:52:33 -0500

    added more env variables for heroku.

--- trakt/settings/prod.py
+++ trakt/settings/prod.py
@@ -18,18 +18,19 @@ DATABASES = {
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASS'],
         'PORT': '5432',
-        'HOST': '/cloudsql/trakt-183713:us-central1:trakt-prod-db-1',
+        'HOST': 'ec2-54-235-220-220.compute-1.amazonaws.com',
     }
 }
 
+# google postrgres HOST = /cloudsql/trakt-183713:us-central1:trakt-prod-db-1
 
-STATIC_URL = 'https://storage.googleapis.com/{}/static/'.format(os.environ['CLOUD_STORAGE_BUCKET'])
+STATIC_URL = 'https://storage.googleapis.com/{}/static/'.format(os.environ['GC_STORAGE_BUCKET'])
 
 # GOOGLE CLOUD
 # ----------------------------------------------------------------------------
 
-PROJECT_ID = os.environ['PROJECT_ID']
-CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+PROJECT_ID = os.environ['GC_PROJECT_ID']
+CLOUD_STORAGE_BUCKET = os.environ['GC_STORAGE_BUCKET']
 MEDIA_PREFIX = "prod/users/"
 
 CLOUD_STORAGE_ROOT = "https://storage.googleapis.com/{bucket_name}/".format(
